04ba8744db7994ef5ee5c69172200a18152e4b61
Orealba <orealbas@gmail.com>
2022-01-27 09:36:44 +0000

    estructura

---
+++ src/js/component/Estructura.jsx
@@ -0,0 +1,11 @@
+import { Linter } from "eslint";
+import "../styles/Estructura.css";
+
+
+const Estructura = () => {
+	return (
+
+	<div className="fondo">
+    </div>      
+    )
+ }
---
+++ src/js/view/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Estructura from "../Component../Estructura.jsx";
+
+const Home = () => {
+	return (
+		<>
+		<Estructura />
+		
+		
+		</>
+
+export default Home;
